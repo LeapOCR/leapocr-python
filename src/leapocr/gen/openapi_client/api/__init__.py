c79c6c87cf1e439a7055a48f28f7183a39ef4978
Spark <79936503+spa5k@users.noreply.github.com>
2025-11-06 22:05:17 +0530

    cleanup

--- src/leapocr/gen/openapi_client/api/__init__.py
+++
@@ -1,6 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.ocr_api import OCRApi
-from openapi_client.api.sdk_api import SDKApi
-from openapi_client.api.upload_api import UploadApi
--- src/leapocr/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-LeapOCR Python SDK - Services Package
-
-This module contains high-level services for specific API features.
-"""
-
-from .ocr import OCRService
-
-__all__ = ["OCRService"]
